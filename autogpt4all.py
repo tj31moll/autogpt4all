86c645dcf7da95994ace50180db5a2110af9823d
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-09-26 15:41:10 -0400

    Update autogpt4all.py for Ubuntu

--- autogpt4all.py
+++ autogpt4all.py
@@ -5,30 +5,34 @@ import os
 import subprocess
 import shutil
 import sys
+
+# Optimized for Proxmox VM with Ubuntu
+print("Detecting distro info...")
+subprocess.run(["uname","-a"])
+
+import argparse
+import os
+import subprocess
+import shutil
+import sys
 from urllib.request import urlretrieve
 
-# Default values
-DEFAULT_MODEL_URL = "https://gpt4all.io/models/ggml-gpt4all-l13b-snoozy.bin"
+# Default values  
+DEFAULT_MODEL_URL = "https://gpt4all.io/models/ggml-gpt4all-l13b-snoozy.bin" 
 PROJECTS = ["LocalAI", "Auto-GPT"]
 AUTO_GPT_BRANCH = "stable"
 
 # Argument parsing
 parser = argparse.ArgumentParser(description="Manage LocalAI and Auto-GPT projects.")
-parser.add_argument(
-    "--custom_model_url",
-    type=str,
-    help="Specify a custom URL for the model download step.",
-)
-parser.add_argument(
-    "--uninstall",
-    action="store_true",
-    help="Uninstall the projects from your local machine.",
-)
+parser.add_argument("--custom_model_url", type=str,  
+                    help="Specify a custom URL for the model download step.")
+parser.add_argument("--uninstall", action="store_true", 
+                    help="Uninstall the projects from your local machine.")
 args = parser.parse_args()
 
 model_url = args.custom_model_url if args.custom_model_url else DEFAULT_MODEL_URL
 
-# Uninstall
+# Uninstall if requested
 if args.uninstall:
     print("Uninstalling...")
     for project in PROJECTS:
@@ -36,12 +40,14 @@ if args.uninstall:
             shutil.rmtree(project)
     sys.exit(0)
 
-# Extra prerequisites for macOS
-if sys.platform == "darwin":
-    subprocess.run(["brew", "install", "cmake", "go"])
-# Extra prerequisites for Linux (assuming apt package manager)
-elif sys.platform == "linux":
-    subprocess.run(["sudo", "apt", "install", "-y", "cmake", "golang"])
+# Install prerequisites
+print("Installing prerequisites...")
+subprocess.run(["sudo", "apt", "update"])  
+subprocess.run(["sudo", "apt", "install", "-y", "git", "cmake", "golang-go"])
+
+# Configure git
+subprocess.run(["git", "config", "--global", "user.name", "'myusername'"])
+subprocess.run(["git", "config", "--global", "user.email", "'my@email.com'"])
 
 # Handle LocalAI
 if not os.path.isdir("LocalAI"):
@@ -50,30 +56,24 @@ else:
     os.chdir("LocalAI")
     subprocess.run(["git", "pull"])
     os.chdir("..")
-
+    
+os.environ["GOOS"] = "linux" 
+os.environ["GOARCH"] = "amd64"
 os.chdir("LocalAI")
-subprocess.run(["make", "build"])
+subprocess.run(["make", "build"])  
 os.chdir("..")
 
-# Only download the model if a custom URL is provided or if the model does not already exist
+# Download model if needed
+if not shutil.which("wget"):
+    subprocess.run(["sudo", "apt", "install", "-y", "wget"])
+
 if args.custom_model_url or not os.path.isfile("LocalAI/models/gpt-3.5-turbo"):
-    if sys.platform == "darwin" or sys.platform.startswith("linux"):
-        subprocess.run(["wget", model_url, "-O", "LocalAI/models/gpt-3.5-turbo"])
-    elif sys.platform == "win32":
-        subprocess.run(["curl", "-o", "LocalAI/models/gpt-3.5-turbo", model_url])
+    subprocess.run(["wget", model_url, "-O", "LocalAI/models/gpt-3.5-turbo"])
 
 # Handle Auto-GPT
 if not os.path.isdir("Auto-GPT"):
-    subprocess.run(
-        [
-            "git",
-            "clone",
-            "-b",
-            AUTO_GPT_BRANCH,
-            "https://github.com/Significant-Gravitas/Auto-GPT.git",
-        ]
-    )
-    shutil.copy(".env.template", "Auto-GPT/.env")
+    subprocess.run(["git", "clone", "-b", AUTO_GPT_BRANCH, "https://github.com/Significant-Gravitas/Auto-GPT.git"])
+    shutil.copy(".env.template", "Auto-GPT/.env") 
 else:
     os.chdir("Auto-GPT")
     subprocess.run(["git", "pull"])
